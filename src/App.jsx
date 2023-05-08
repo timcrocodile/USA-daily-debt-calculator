ad5a61278a75ee08307d3afe33fa69c96747ffba
timcrocodile <timcrocodile@proton.me>
2023-05-08 23:52:39 +0200

    2.5d

--- src/App.jsx
+++ src/App.jsx
@@ -91,29 +91,52 @@ function App() {
 
   return (
     <>
-      <div className="container">
-        <div>
-          <p className="differencetext">
-            {differenceText}{" "}
-            <span onClick={handleAsteriskClick} style={{ cursor: "pointer" }}>
-              (*)
-            </span>
-          </p>{" "}
-          {showMessage && (
-            <div>
-              <p className="alert">
-                * The measurement was carried out by comparing the most recent
-                data available, which is usually from two days prior (t-2).
-              </p>
-            </div>
-          )}
-        </div>
+      <div className="totalcontainer">
+        <div className="container">
+          <div>
+            <p className="differencetext">
+              {differenceText}{" "}
+              <span onClick={handleAsteriskClick} style={{ cursor: "pointer" }}>
+                (*)
+              </span>
+            </p>{" "}
+            {showMessage && (
+              <div>
+                <p className="alert">
+                  * The measurement was carried out by comparing the most recent
+                  data available, which is usually from two days prior (t-2).
+                </p>
+              </div>
+            )}
+          </div>
 
-        <PreviousFirst onDebtHeld={handleDebtHeld} />
-        <Previous onDebtHeld2={handleDebtHeld2} />
-        <Previous2 onDebtHeld3={handleDebtHeld3} />
-        <Previous3 onDebtHeld4={handleDebtHeld4} />
-        <Previous4 onDebtHeld5={handleDebtHeld5} />
+          <PreviousFirst onDebtHeld={handleDebtHeld} />
+          <Previous onDebtHeld2={handleDebtHeld2} />
+          <Previous2 onDebtHeld3={handleDebtHeld3} />
+          <Previous3 onDebtHeld4={handleDebtHeld4} />
+          {/* <Previous4 onDebtHeld5={handleDebtHeld5} /> */}
+        </div>
+        <img
+          className="king"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/ecba78169166509.6449830728077.gif"
+          alt="king"
+          width="430"
+          height="620"
+        />{" "}
+        <img
+          className="samurai"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/a50690169579513.644f901c144e1.gif"
+          alt="3d"
+        />
+        <img
+          className="superman"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2be692169168581.64498074cde5e.gif
+          "
+          alt="superman"
+          width="230"
+          height="320"
+          opacity="0.5"
+        />
       </div>
     </>
   );
